a2f34f6656f03d3638e345d3b564bbdff82f501f
ThBarroso <130921771+ThBarroso@users.noreply.github.com>
2024-03-07 16:09:04 -0300

    Add files via upload

---
+++ Ti1numint/src/numint.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class numint {
+	public static void main(String args[]) {
+		Scanner ler = new Scanner(System.in);
+		int num1=ler.nextInt();
+		int num2=ler.nextInt();
+		int soma=num1+num2;
+		
+		
+	}
+
+}
